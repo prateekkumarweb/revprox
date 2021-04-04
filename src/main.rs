c8b584e479964fbf9045becd48ffa7601b16a567
Prateek Kumar <prateek@prateekkumar.in>
2021-04-04 16:13:12 +0530

    fix issue with url path
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use hyper::{
     header,
+    http::uri,
     service::{make_service_fn, service_fn},
-    Body, Client, Request, Response, Server,
+    Body, Client, Request, Response, Server, Uri,
 };
 use std::{collections::HashMap, convert::Infallible, net::SocketAddr, sync::Arc};
 use structopt::StructOpt;
@@ -13,16 +14,33 @@ async fn handle_client(
     req: Request<Body>,
     servers_map: Arc<HashMap<String, String>>,
 ) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
+    dbg!(&req);
+
     let client = Client::new();
     let host = req.headers().get(header::HOST);
-    let uri = host
+
+    let uri: Uri = host
         .and_then(|host| host.to_str().ok())
         .and_then(|host| servers_map.get(host))
         .and_then(|uri| uri.parse().ok())
         .unwrap_or("http://127.0.0.1:8000/".parse()?);
+
+    let uri_parts = uri.into_parts();
+    let uri_builder = Uri::builder()
+        .scheme(uri_parts.scheme.unwrap_or(uri::Scheme::HTTP))
+        .authority(uri_parts.authority.unwrap())
+        .path_and_query((req.uri().path_and_query().unwrap()).clone());
+
     let mut new_req = Request::from(req);
-    *new_req.uri_mut() = uri;
+
+    *new_req.uri_mut() = uri_builder.build()?;
+
+    dbg!(&new_req);
+
     let resp = client.request(new_req).await?;
+
+    dbg!(&resp);
+
     Ok(resp)
 }
 
