da6339b1ba437787437691c72f4e1ce62054bb30
Prateek Kumar <prateek@prateekkumar.in>
2021-04-09 22:23:24 +0530

    basic implementation of https
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+use futures_util::stream::Stream;
 use handler::Handler;
 use hyper::{
-    server::conn::AddrStream,
     service::{make_service_fn, service_fn},
     Server,
 };
-use std::{convert::Infallible, net::SocketAddr, sync::Arc};
+use std::{
+    convert::Infallible,
+    fs::File,
+    io::BufReader,
+    net::SocketAddr,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
 use structopt::StructOpt;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_rustls::{
+    rustls::{
+        internal::pemfile::{certs, pkcs8_private_keys},
+        NoClientAuth, ServerConfig,
+    },
+    server::TlsStream,
+    TlsAcceptor,
+};
 
 mod handler;
 mod opt;
@@ -18,9 +35,28 @@ async fn main() {
     let handler = Handler::new(settings.servers());
     let handler = Arc::new(handler);
 
-    let make_service = make_service_fn(|conn: &AddrStream| {
+    let cert = certs(&mut BufReader::new(File::open("./cert.pem").unwrap())).unwrap();
+    let mut keys =
+        pkcs8_private_keys(&mut BufReader::new(File::open("./key.pem").unwrap())).unwrap();
+
+    let mut server_config = ServerConfig::new(NoClientAuth::new());
+    server_config.set_single_cert(cert, keys.remove(0)).unwrap();
+    let tls_acceptor = TlsAcceptor::from(Arc::new(server_config));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], opt.port));
+    let tcp = TcpListener::bind(&addr).await.unwrap();
+
+    let incoming_tls_stream = async_stream::stream! {
+        loop {
+            let (socket, _) = tcp.accept().await.unwrap();
+            let stream = tls_acceptor.accept(socket);
+            yield stream.await;
+        }
+    };
+
+    let make_service = make_service_fn(|conn: &TlsStream<TcpStream>| {
         let handler = handler.clone();
-        let addr = conn.remote_addr();
+        let addr = conn.get_ref().0.peer_addr().unwrap();
         async move {
             Ok::<_, Infallible>(service_fn(move |req| {
                 let handler = handler.clone();
@@ -29,12 +65,30 @@ async fn main() {
         }
     });
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], opt.port));
-    let server = Server::bind(&addr).serve(make_service);
+    let server = Server::builder(HyperAcceptor {
+        acceptor: Box::pin(incoming_tls_stream),
+    })
+    .serve(make_service);
 
     println!("Listening on port {}", opt.port);
 
     if let Err(e) = server.await {
         eprintln!("Server error: {}", e);
+    }
+}
+
+struct HyperAcceptor<'a> {
+    acceptor: Pin<Box<dyn Stream<Item = Result<TlsStream<TcpStream>, std::io::Error>> + 'a>>,
+}
+
+impl hyper::server::accept::Accept for HyperAcceptor<'_> {
+    type Conn = TlsStream<TcpStream>;
+    type Error = std::io::Error;
+
+    fn poll_accept(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+    ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
+        Pin::new(&mut self.acceptor).poll_next(cx)
     }
 }
